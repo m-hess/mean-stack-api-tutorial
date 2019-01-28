9b09f8dc703789feee804f0f91cbc09338c9dd92
m-hess <madeline.e.hess.19@dartmouth.edu>
2019-01-28 17:49:02 -0500

    test beep boop

--- server.js
+++ server.js
@@ -41,6 +41,12 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
+
+
+app.get("/", function(req, res) {
+  console.log("beep boop!")
+});
+
 /*  "/api/contacts"
  *    GET: finds all contacts
  *    POST: creates a new contact
